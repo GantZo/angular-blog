5cd8e760b16ce178308b5f0f9c103d3de4f5c59e
GantZo <putrify@mail.ru>
2021-07-12 12:22:16 +0300

    admin auth.service login

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,7 +1,9 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
-import {User} from "../../../shared/interfaces";
+import {FbAuthResponse, User} from "../../../shared/interfaces";
 import {Observable} from "rxjs";
+import {environment} from "../../../../environments/environment";
+import {map, tap} from "rxjs/operators";
 
 @Injectable()
 export class AuthService {
@@ -12,8 +14,11 @@ export class AuthService {
     return ''
   }
 
-  login(user: User): Observable<any> {
-    return  this.http.post('', user)
+  login(user: User): Observable<FbAuthResponse> {
+    return  this.http.post<FbAuthResponse>(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+      .pipe(
+        tap(this.setToken)
+      )
   }
 
   logout() {
@@ -24,7 +29,8 @@ export class AuthService {
     return !!this.token
   }
 
-  private setToken() {
-
+  private setToken(response: FbAuthResponse) {
+    console.log(response)
+    console.log(response.idToken)
   }
 }
