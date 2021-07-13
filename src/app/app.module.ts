0220321f9a92a9587f7d656cdc893acdbb8bbdca
GantZo <putrify@mail.ru>
2021-07-13 17:13:55 +0300

    add pwa not configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {LOCALE_ID, NgModule, Provider} from '@angular/core';
+import {NgModule, Provider} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {registerLocaleData} from "@angular/common";
 import ruLocale from '@angular/common/locales/ru'
@@ -12,6 +12,8 @@ import {PostComponent} from './shared/components/post/post.component';
 import {SharedModule} from "./shared/shared.module";
 import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {AuthInterceptor} from "./shared/auth.interceptor";
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
 
 registerLocaleData(ruLocale, 'ru')
 
@@ -32,7 +34,13 @@ const INTERCEPTOR_PROVIDER: Provider = {
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [INTERCEPTOR_PROVIDER],
   bootstrap: [AppComponent]
